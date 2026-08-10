a87d8a040d68e2c1b0f54789c68222eeaa522135
hamidrza0008 <hamidrza0008@gmail.com>
2026-08-10 06:22:59 +0530

    Q 10

--- Basics/Basic.js
+++ Basics/Basic.js
@@ -1,24 +1,24 @@
 // 1. Write a Function that searchs for an element in an array and returns its index ,  if the elment is not presnet then just return -1
 
-let arr = [1,4,8,9,4,7]
-function searchElement(arr , x){
-    for(let i=0 ; i<arr.length ; i++){
-        if(arr[i] == x){
+let arr = [1, 4, 8, 9, 4, 7]
+function searchElement(arr, x) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == x) {
             return i;
         }
     }
     return -1
 }
-let result = searchElement(arr , 4);
+let result = searchElement(arr, 4);
 console.log(result);
 
 // 2. WAP that returns the counts of all negetive numbers occured in an array
 
-function countNegative(arr){
+function countNegative(arr) {
     let count = 0;
-    for(let i=0 ; i<arr.length ; i++){
-        if(arr[i]<0){
-            count = count+1;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            count = count + 1;
         }
     }
     return count
@@ -28,78 +28,78 @@ console.log(result);
 
 // 3. Find The Largest Number in an Array
 
-function findLargest(arr){
+function findLargest(arr) {
     let max = -1; // This will not work for array containinig NEGATIVE Numbers 
-     for(let i=0 ; i<arr.length ; i++){
-         if(max<arr[i]){
-             max = arr[i];
-         }
-     }
-     return max;
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 let result = findLargest(arr);
-console.log(result);            
+console.log(result);
 
 //This another Solution 
-function findLargest(arr){
-    let max = -Infinity ; // this will work for find all largest number 
-     for(let i=0 ; i<arr.length ; i++){
-         if(max<arr[i]){
-             max = arr[i];
-         }
-     }
-     return max;
+function findLargest(arr) {
+    let max = -Infinity; // this will work for find all largest number 
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 let result = findLargest(arr);
 console.log(result);
 
 
-function findLargest(arr){
-    let max = arr[0] ; // this is the best approach 
-     for(let i=0 ; i<arr.length ; i++){
-         if(max<arr[i]){
-             max = arr[i];
-         }
-     }
-     return max;
+function findLargest(arr) {
+    let max = arr[0]; // this is the best approach 
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 let result = findLargest(arr);
 console.log(result);
 
 // 4. Find the Smallest Number in an Array
 
-function findLargest(arr){
-    let max = arr[0] ;
-     for(let i=0 ; i<arr.length ; i++){
-         if(max>arr[i]){
-             max = arr[i];
-         }
-     }
-     return max;
+function findLargest(arr) {
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (max > arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 let result = findLargest(arr);
 console.log(result);
 
 // 5. Find The SecondLargest Number in an Array
 
-function secondLargest(arr){
+function secondLargest(arr) {
     let firstLargest = -Infinity;
     let secondLargest = -Infinity;
-    
-    for(let i=0 ; i<arr.length ; i++){
-        if(firstLargest < arr[i]){
+
+    for (let i = 0; i < arr.length; i++) {
+        if (firstLargest < arr[i]) {
             secondLargest = firstLargest;
             firstLargest = arr[i];
         }
-        else if(secondLargest < arr[i]){
+        else if (secondLargest < arr[i]) {
             secondLargest = arr[i];
         }
     }
-    
+
     return secondLargest;
 }
 
-let arr = [-3,4,-1,-10];
+let arr = [-3, 4, -1, -10];
 let result = secondLargest(arr);
 console.log(result);
 
@@ -108,28 +108,28 @@ console.log(result);
 //Dupplicate Numbers
 // So We have to make it More Efficient
 
-function secondLargest(arr){
+function secondLargest(arr) {
     let firstLargest = -Infinity;
     let secondLargest = -Infinity;
-    
-    if(arr.length < 2){
+
+    if (arr.length < 2) {
         return "Invalid Array";
     }
-    
-    for(let i=0 ; i<arr.length ; i++){
-        if(firstLargest < arr[i]){
+
+    for (let i = 0; i < arr.length; i++) {
+        if (firstLargest < arr[i]) {
             secondLargest = firstLargest;
             firstLargest = arr[i];
         }
-        else if(secondLargest < arr[i] && arr[i] != firstLargest){
+        else if (secondLargest < arr[i] && arr[i] != firstLargest) {
             secondLargest = arr[i];
         }
     }
-    
+
     return secondLargest;
 }
 
-let arr = [1,4,5,2,4,7,8,8,0];
+let arr = [1, 4, 5, 2, 4, 7, 8, 8, 0];
 // let arr = [1,2]
 
 let result = secondLargest(arr);
@@ -137,11 +137,11 @@ console.log(result);
 
 // 6. WAP to make a star pattern of n by n
 
-function makePattern(n){
-    for(let i = 0; i < n; i++){
+function makePattern(n) {
+    for (let i = 0; i < n; i++) {
         let row = "";
 
-        for(let j = 0; j < n; j++){
+        for (let j = 0; j < n; j++) {
             row += "* ";
         }
 
@@ -151,26 +151,26 @@ function makePattern(n){
 
 makePattern(4); // it will give one undiefined output at last
 
-n=4  // you can give any numbers
-    for(let i=0 ; i<n ; i++){
-        let row ="";
-        for(let j=0 ; j<n ; j++){
-            row = row+"* ";
-        }
-        console.log(row);
+n = 4  // you can give any numbers
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+        row = row + "* ";
+    }
+    console.log(row);
 }
 
 // Here is the best Solution
-function makePatterns(n){
+function makePatterns(n) {
     let pattern = "";
-    for(let i=0 ; i<n ; i++){
-        let row ="";
-        for(let j=0 ; j<n ; j++){
-            row = row+"* ";
+    for (let i = 0; i < n; i++) {
+        let row = "";
+        for (let j = 0; j < n; j++) {
+            row = row + "* ";
         }
         pattern = pattern + row + "\n";
     }
-    
+
     return pattern
 }
 console.log(makePatterns(4));
@@ -183,20 +183,20 @@ console.log(makePatterns(4));
 // * * * 
 // * * * * 
 
-function makePatterns(n){
+function makePatterns(n) {
     let pattern = "";
-    
-    for(let i=0 ; i<n ; i++){
+
+    for (let i = 0; i < n; i++) {
         let row = "";
-        for(let j=0 ; j<i+1 ; j++){
+        for (let j = 0; j < i + 1; j++) {
             row = row + "* ";
         }
         pattern += row + "\n";
     }
-    
+
     return pattern
 }
-    
+
 
 console.log(makePatterns(4));
 
@@ -207,20 +207,20 @@ console.log(makePatterns(4));
 // 4 4 4 4 
 // 5 5 5 5 5 
 
-function makePatterns(n){
+function makePatterns(n) {
     let pattern = "";
-    
-    for(let i=1 ; i<=n ; i++){
+
+    for (let i = 1; i <= n; i++) {
         let row = "";
-        for(let j=1 ; j<=i ; j++){
-            row+= j+" ";
+        for (let j = 1; j <= i; j++) {
+            row += j + " ";
         }
         pattern += row + "\n";
     }
-    
+
     return pattern
 }
-    
+
 
 console.log(makePatterns(5));
 
@@ -232,20 +232,40 @@ console.log(makePatterns(5));
 // 4 4 4 4 
 // 5 5 5 5 5 
 
-function makePatterns(n){
+function makePatterns(n) {
     let pattern = "";
-    
-    for(let i=1 ; i<=n ; i++){
+
+    for (let i = 1; i <= n; i++) {
         let row = "";
-        for(let j=1 ; j<=i ; j++){
-            row+= i+" ";
+        for (let j = 1; j <= i; j++) {
+            row += i + " ";
         }
         pattern += row + "\n";
     }
-    
+
     return pattern
 }
-    
+
 
 console.log(makePatterns(5));
 
+// 10. Pattern Like this
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
+
+function makePattern(n) {
+    let pattern = "";
+    for (let i = 0; i <= n; i++) {
+        let row = "";
+        for (let j = 1; j <= n - i; j++) {
+            row += j + " ";
+        }
+        pattern += row + "\n";
+    }
+    return pattern;
+}
+
+console.log(makePattern(5));
