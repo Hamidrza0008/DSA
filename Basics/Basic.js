f351aa10b68d08318ff43417b924b5d4d5028ef2
hamidrza0008 <hamidrza0008@gmail.com>
2026-08-29 23:22:34 +0530

    Q 32

--- Basics/Basic.js
+++ Basics/Basic.js
@@ -734,3 +734,24 @@ function isArmstrong(num){
 }
 
 console.log(isArmstrong(num))
+
+// 32. Do arrays ka intersection find karo.
+let arr1 = [1,2,3,4]
+let arr2 = [3,4,5,6]
+//→ [3,4]
+
+function findIntersection(arr1 , arr2){
+    let inter = [];
+    for(let i=0 ; i<arr1.length ; i++){
+        for(let j=0 ; j<arr2.length ; j++){
+            if(arr1[i] == arr2[j]){
+                inter.push(arr1[i]);
+            }
+        }
+    }
+
+    return inter;
+}
+
+console.log(findIntersection(arr1 , arr2))
+
